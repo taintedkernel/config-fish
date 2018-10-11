c22a9d151b66ec6879e185462cafd8cf89724bbf
Anthony DeChiaro <anthonyd@unity3d.com>
2018-10-11 13:57:27 -0700

    Setting key lifetime instead of immediate removal

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -183,9 +183,8 @@ end
 # Assumes there is a correct key to match ~/.ssh/id_github*
 function gsomk --description "git push origin master with temporary unlocked key"
     set KEY (ls -1 $HOME/.ssh/id_github* | grep -v '\.pub')
-    ssh-add $KEY
+    ssh-add -t 600 $KEY
     git push origin master
-    ssh-add -d $KEY
 end
 
 # vcsh #
