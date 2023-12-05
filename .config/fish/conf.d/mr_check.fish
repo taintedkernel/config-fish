2cf759ab1507004f7c2f5198979310019183ed20
Anthony DeChiaro <taintedkernel@gmail.com>
2023-12-05 11:08:33 -0800

    Hide mr errors if nothing is configured

--- .config/fish/conf.d/mr_check.fish
+++ .config/fish/conf.d/mr_check.fish
@@ -2,7 +2,7 @@
 
 set MR (which mr)
 if [ "$status" = 0 -a -x "$MR" -a "$HOME/.mrconfig" ]
-    cd && mr status | awk 'BEGIN { repo = "" }
+    cd && mr status 2>/dev/null | awk 'BEGIN { repo = "" }
         /mr status:/ { if (count > 0) changes[repo] = count; repo = $3; count = 0; }
         / M / { count++ }
         END { if (length(changes) > 0) { print "Repositories with changes:";\
