55f81ed3670f26986348682b408a8d8dd5181a05
Anthony DeChiaro <taintedkernel@gmail.com>
2023-12-18 14:22:40 -0800

    Fix file check, add tty check

--- .config/fish/conf.d/mr_check.fish
+++ .config/fish/conf.d/mr_check.fish
@@ -1,7 +1,7 @@
 # Check for changes or updates to mr/vcsh repositories
 
 set MR (which mr)
-if [ "$status" = 0 -a -x "$MR" -a "$HOME/.mrconfig" ]
+if [ "$status" = 0 -a -x "$MR" -a -f "$HOME/.mrconfig" -a (isatty) ]
     cd && mr status 2>/dev/null | awk 'BEGIN { repo = "" }
         /mr status:/ { if (count > 0) changes[repo] = count; repo = $3; count = 0; }
         / M / { count++ }
